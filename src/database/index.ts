0cfdf9d447271075fa333eee414db5dfb20af11a
Gabriel Cesario <cesario203@outlook.com>
2021-03-06 14:32:11 -0300

    skln

--- src/database/index.ts
+++ src/database/index.ts
@@ -11,6 +11,15 @@ export default async ():Promise<Connection> =>
             newObj = {
                 type:'postgres',
                 url:process.env.DATABASE_URL,
+                migrations:
+    [
+        "./dist/database/migrations/**.js"
+    ],
+    entities:["./dist/models/**.js"],
+    cli:
+    {
+        migrationsDir:"./dist/database/migrations"
+    },
                 ssl:{rejectUnauthorized:false}
             }
             console.log(defaultOptions)
